a471855268e552e863d96ac66ead70ced1495c1b
Kevin Knights <kevinknights29@hotmail.com>
2025-01-01 16:39:26 -0500

    refactor: updated paths

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from youtube_history_analyzer.cli import process_watch_history
 
 if __name__ == "__main__":
-    input_file = "data/watch-history.html"
-    output_file = "data/watch-history.csv"
+    input_file = "data/input/watch-history.html"
+    output_file = "data/output/watch-history.csv"
 
     process_watch_history(input_file, output_file)
